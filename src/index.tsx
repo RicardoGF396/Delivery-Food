e0f83c96526484a7b1841d6cdfbd3e5925f3e9b7
RicardoGF396 <ricardogf396@gmail.com>
2022-03-05 17:48:27 -0600

    Comentario en index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//ESTO ES UN COMENTARIO --------------------------------
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
